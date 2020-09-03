4d15d75ffbcdab08ef12e144ea1e8e75695fac03
Lima1005 <68686100+Lima1005@users.noreply.github.com>
2020-09-03 10:05:31 +0300

    first commit

--- code.js
+++ code.js
@@ -1,16 +1,9 @@
-// При нажатии на кнопку пропадает панель с куками
-let cookie__wrapper = document.querySelector(".cookie__wrapper");
-let cookie__wrapper__button = document.querySelector(".cookie__wrapper__button");
 
 let promo__wrapper_btn = document.querySelector(".promo__wrapper_btn")
 let wrapper_form = document.querySelector(".wrapper_form")
 // let form= document.querySelector("form");
 let cross = document.querySelector("i")
 
-
-cookie__wrapper__button.onclick = () =>{
-    cookie__wrapper.style.display = "none";
-}
 
 promo__wrapper_btn.onclick = () => {
     wrapper_form.style.display = "block";
